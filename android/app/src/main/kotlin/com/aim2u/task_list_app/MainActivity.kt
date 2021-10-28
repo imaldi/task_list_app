e932529446efc952df60a1ceb0536e8cb8cba087
imaldi <aldiirsanmajid@gmail.com>
2021-10-28 11:08:11 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/aim2u/task_list_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aim2u.task_list_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'data/moor_database.dart';
+import 'ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      // The single instance of AppDatabase
+      // builder: (_) => ,
+      create: (BuildContext context) { AppDatabase(); },
+      child: MaterialApp(
+        title: 'Material App',
+        home: HomePage(),
+      ),
+    );
+  }
+}
